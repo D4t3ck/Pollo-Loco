4ccd6dda4ffaebf712569735531332a95958ab6a
Daniel Sinteck <d.sint3ck@gmail.com>
2024-02-22 15:30:25 +0100

    added full background img

--- models/background_object.class.js
+++ models/background_object.class.js
@@ -2,7 +2,7 @@ class BackgroundObject extends MovableObject {
     
 
   width = 720;
-  height = 300;
+  height = 480;
 
   constructor(imagePath, x) {
     super().loadImage(imagePath);
--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,5 @@
 class Character extends MovableObject {
+  y = 280;
 
     constructor() {
         super().loadImage('./img/2_character_pepe/2_walk/W-21.png')
