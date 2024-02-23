f6533f1a1fc4253aafc8df08db27751dbaa89bce
Daniel Sinteck <d.sint3ck@gmail.com>
2024-02-23 12:22:36 +0100

    added animation for character and chicken

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,18 @@
 class Cloud extends MovableObject {
-  y = 20;
+  y = 80;
   width = 500;
-  height = 250;
+  height = 300;
 
   constructor() {
     super().loadImage("./img/5_background/layers/4_clouds/1.png");
 
     this.x = Math.random() * 500;
-    
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -5,10 +5,24 @@ class MovableObject {
   img;
   height = 150;
   width = 100;
+  imageCache = [];
+  currentImage = 0;
 
   loadImage(path) {
     this.img = new Image(); // == this.img = document.getElementById('image') <img id="image">
     this.img.src = path;
+  }
+
+  /**
+   * 
+   * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+    let img = new Image();
+    img.src = path;
+    this.imageCache[path] = img;
+  });
   }
 
   moveRight() {
