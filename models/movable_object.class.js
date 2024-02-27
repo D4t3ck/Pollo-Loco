5360c1de0d35e1545f064ebbeec247aff53ee946
Daniel Sinteck <d.sint3ck@gmail.com>
2024-02-27 20:55:10 +0100

    ++ collision detection, hurt & dead images

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -11,6 +11,7 @@ class MovableObject {
   otherDirection = false;
   speedY = 0;
   acceleration = 2.5;
+  energy = 100;
 
   applyGravity() {
     setInterval(() => {
@@ -28,6 +29,41 @@ class MovableObject {
   loadImage(path) {
     this.img = new Image(); // == this.img = document.getElementById('image') <img id="image">
     this.img.src = path;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "3";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
+  }
+
+  // character.isColliding(chicken);
+  isColliding(mo) {
+    return (
+      this.x + this.width > mo.x &&
+      this.y + this.height > mo.y &&
+      this.x < mo.x &&
+      this.y < mo.y + mo.height
+    );
+  }
+
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    }
+  }
+
+  isDead() {
+    return this.energy == 0;
   }
 
   /**
@@ -61,3 +97,18 @@ class MovableObject {
     this.speedY = 30;
   }
 }
+
+// if (
+//   character.x + characcter.width > chicken.x &&
+//   character.y + character.height > chicken.y &&
+//   character.x < chicken.x &&
+//   characcter.y < chicken.y + moveBy.height
+// );
+
+// isColliding (obj) {
+//   return  (this.X + this.width) >= obj.X && this.X <= (obj.X + obj.width) &&
+//           (this.Y + this.offsetY + this.height) >= obj.Y &&
+//           (this.Y + this.offsetY) <= (obj.Y + obj.height) &&
+//           obj.onCollisionCourse; // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
+
+// }
